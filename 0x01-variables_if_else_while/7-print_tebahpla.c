37296677f4bd293a59d1f788ee458364338a6646
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-09 23:57:46 -0700

    reverse alph

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Reverse letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+	for (k = 122; k > 96; k--){
+		putchar(k);
+	} putchar('\n');
+	return(0);
+}
