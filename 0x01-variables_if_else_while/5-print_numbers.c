ebc040a0fdad29e0ae3d1c4408a786150411093b
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-09 23:37:02 -0700

    1-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers 1-10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 9; i++){
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
