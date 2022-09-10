023c507dc7d85864dadd68c6e0c3b8d4b83a0ad5
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-10 00:31:15 -0700

    commas and spaces with nums

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 - 9 with comma and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+	for (k = 48; k < 58; k++)
+	{
+	putchar(k);
+	if (k != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
