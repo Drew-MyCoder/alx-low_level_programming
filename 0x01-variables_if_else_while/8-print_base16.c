aa7ea6937f5dfd0cb889963faeddc6b5674cc867
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-10 00:23:50 -0700

    nums and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 - 9and letters a - f
+ *
+ * Return: Always 0 (Success))
+ */
+int main(void)
+{
+	int k;
+	for (k= 48; k < 58; k++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar('\n');
+	return (0);
+}
