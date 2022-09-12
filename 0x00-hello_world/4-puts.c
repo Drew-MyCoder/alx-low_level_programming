65babe97e84b5885363c8c202dbd7341c4f5a078
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-11 21:47:39 -0700

    statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Description: A C program that prints with put function
  *
- * Retrun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts(\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
