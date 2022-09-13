2f546c13c789e16ae4d08b05fb5874327838c952
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-12 21:22:19 -0700

    print alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = 
+	"abcdefghijklmnopqrstuvwxyz";
 	int k;
-	for (k = 0; k <= 26; k++)
+
+	for (k = 0; k < 26; k++)
 	{
-	putchar(alp[k]);
+		putchar(alp[k]);
 	}
 	putchar('\n');
 	return (0);
