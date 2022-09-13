fec6ad317a24f5be4f78c5c4280ffc778c85ff5b
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-12 22:07:30 -0700

    nums, space and cooma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int k;
+
 	for (k = 48; k < 58; k++)
 	{
-	putchar(k);
-	if (k != 57)
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		putchar(k);
+		if (k != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
