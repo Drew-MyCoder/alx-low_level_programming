c657c6a18faad30c883b5576f18134d53db0b086
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-20 13:43:54 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse of a string
+ * @s the string that is reversed
+ */
+void rev_string(char *s)
+{
+	int l = 0, i = 0;
+	char temp;
+
+	while (s[i++])
+		l++;
+
+	for (i = l -1; i >= l / 2; i--)
+	{
+		temp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i -1] = temp;
+	}
+}
