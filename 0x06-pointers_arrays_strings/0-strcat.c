2b70521f6d5b056f96ee1daab5e63d602ea6b8ef
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-21 12:32:39 -0700

    task0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strcat - Concat the string pointed to by @src to include the terminating null bytr
+ * @dest: A pointer to the string to be concatenated
+ * @src: The source string to be appended 
+ *
+ * Return: A pointer to the destination string @dest
+ */
+char *strcat(char *dest, const char *src)
+{
+	/* concat two strings */
+	int i = 0, length_dest = 0;
+
+	while (dest[i++])
+		length_dest++;
+
+	for (i = 0; src[i]; i++)
+		dest[length_dest++] = src[i];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Code for pointer and arrays
