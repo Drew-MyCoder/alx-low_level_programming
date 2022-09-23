246fa229a2d5f50d88b2d387351487c5fe1d1dff
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-22 22:14:05 -0700

    leet file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string to 1337
- * @str: The string to be encoded
- *
- * Return: A posinter to the encoded string
+ * leet - encodes a string into 1337
+ * @s: input string
+ * Return: the pointer to dest
  */
 char *leet(char *s)
 {
-	/* to encode string */
-	int i = 0, j;
+	/* 1337 */
+	int c = 0, j;
 	int low_let[] = {97, 101, 111, 116, 108};
-	int up_let[] = {65, 69, 70, 84, 76};
+	int up_let[]  = {65, 69, 79, 84, 76};
 	int nums[] = {52, 51, 48, 55, 49};
-	
-	while (*(s + i) != '\0')
+
+	while (*(s + c) != '\0')
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (*(s + i == low_let[j] || *(s + i) == up_let[j])
-					{
-						*(s + i) = nums[j];
-						break;
-					}
+			if (*(s + c) == low_let[j] || *(s + c) == up_let[j])
+			{
+				*(s + c) = nums[j];
+				break;
+			}
 		}
-		i++;
+		c++;
 	}
 
 	return (s);
