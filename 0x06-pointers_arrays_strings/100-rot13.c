9ca31c12e122778564bacf4534c0ab979a7f18ce
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2022-09-22 22:23:26 -0700

    rot file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,47 +1,31 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string using rot13
- * @str: The string to be encoded
- *
- * Return: A pointer to the encoded string
+ * rot13 - encodes s string using rot13
+ * @s: input string
+ * Return: the pointer to dest
  */
-char *rot13(char *str)
-{
-	/* string */
-	int i = 0, j;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		'G', 'H', 'I', 'J', 'K', 'L',
-		'M', 'N', 'O', 'P', 'Q', 'R',
-		'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y',
-		'Z', 'A', 'B', 'C', 'D', 'E',
-		'F', 'G', 'H', ;'I', 'J', 'K',
-		'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c',
-		'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k', 'l', 'm'};
 
-	while (str[i])
+char *rot13(char *s)
+{
+	/* use rot 13 */
+	int cont = 0, j;
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklnNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (str[i] == alphabet[j])
+			if (*(s + count) == alph[j])
 			{
-				str[i] = rot13key[j];
+				*(s + count) = rot13[j];
 				break;
 			}
 		}
-
-		i++;
+		count++;
 	}
 
-	return (str);
+	return (s);
 }
+
